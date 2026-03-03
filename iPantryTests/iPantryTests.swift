63ab3f42fae8adeb5f12e45e3ea5dd63a99b5ad3
Academy <rafaelraja.sitorus@gmail.com>
2026-03-03 15:16:20 +0700

    feat: initial commit

---
+++ iPantryTests/iPantryTests.swift
@@ -0,0 +1,17 @@
+//
+//  iPantryTests.swift
+//  iPantryTests
+//
+//  Created by Academy on 03/03/26.
+//
+
+import Testing
+@testable import iPantry
+
+struct iPantryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
